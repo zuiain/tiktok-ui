b59e1fa4ddeea76394f32ab5515f38ff6f120112
Jack Bottom <cross123skull@gmail.com>
2023-11-10 06:30:16 +0700

    update-css

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,9 +1,8 @@
+import { Outlet } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 import Header from '../components/Header';
 import Sidebar from './Sidebar';
-import { Outlet } from 'react-router-dom';
-import styles from './DefaultLayout.module.scss';
-import classNames from 'classnames/bind';
-
 const cx = classNames.bind(styles);
 
 function DefaultLayout() {
